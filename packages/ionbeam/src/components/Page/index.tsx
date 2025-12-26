a59ca735cf50c12a10ac26893bd762bded627141
Martin Podloucký <podlouckymartin@gmail.com>
2025-12-26 15:47:39 +0100

    Add htmx script to Page component

--- packages/ionbeam/src/components/Page/index.tsx
+++ packages/ionbeam/src/components/Page/index.tsx
@@ -15,6 +15,8 @@ export const Page = ({ children, title }: Props): JSX.Element => {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{title}</title>
+        <script src="https://cdn.jsdelivr.net/npm/htmx.org@2.0.8/dist/htmx.min.js"></script>
+
         {styleSheet && <link rel="stylesheet" href={`/${styleSheet}`} />}
         {clientScript && <script src={`/${clientScript}`} defer />}
       </head>
